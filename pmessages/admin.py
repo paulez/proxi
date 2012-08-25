59b123804e45c861d851fbb2adedfedc409ad486
Paul Ezvan <paul@ezvan.fr>
2012-08-25 23:13:57 +0200

    Improving messages display in admin.

--- pmessages/admin.py
+++ pmessages/admin.py
@@ -1,4 +1,14 @@
 from django.contrib.gis import admin
 from pmessages.models import ProxyMessage
 
-admin.site.register(ProxyMessage, admin.OSMGeoAdmin)
+class ProxyMessageAdmin(admin.OSMGeoAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['message']}),
+        ('Message information', {'fields': ['username', 'address', 'location', 'ref', 'priority'], 'classes': ['collapse']}),
+        ]
+    list_display = ('message', 'username')
+    list_filter = ['date']
+    search_field = ['message']
+
+admin.site.register(ProxyMessage, ProxyMessageAdmin)
+
