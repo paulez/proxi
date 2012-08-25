2553b93f5928bc8c23cfb970593247a180e0d488
Paul Ezvan <paul@ezvan.fr>
2012-08-25 21:20:51 +0200

    Add priority attribute to messages.

--- pmessages/models.py
+++ pmessages/models.py
@@ -3,13 +3,21 @@ from django.contrib.gis.db import models
 class ProxyMessage(models.Model):
     """A ProxyMessage is a simple text message with location information, username, originated ip and date.
     It may be extended in the future to support multimedia attached files."""
+    # username chosen by the message sender
     username = models.CharField(max_length=20)
+    # message content
     message = models.TextField()
+    # creation date of the message
     date = models.DateField(auto_now_add=True)
+    # ip address of the message sender
     address = models.GenericIPAddressField()
+    # location of the message sender
     location = models.PointField()
     objects = models.GeoManager()
+    # Reference to a parent message, NULL if no parent
     ref = models.ForeignKey('self', null=True)
+    # message priority
+    priority = models.PositiveSmallIntegerField(default=0)
     
     def __unicode__(self):
         return self.message
