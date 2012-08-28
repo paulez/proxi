9f7b0c94572708bd2c0523218b3d6e7f8b040b3b
Paul Ezvan <paul@ezvan.fr>
2012-08-28 23:02:02 +0200

    View drafts.

--- pmessages/views.py
+++ pmessages/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
+from pmessages.models import ProxyMessage
+
+def index(request):
+    all_messages = ProxyMessage.objects.all().order_by('-date')
+    return render_to_response('pmessages/index.html', {'all_messages': all_messages,})
+    
+def detail(request, message_id):
+    m = get_object_or_404(ProxyMessage, pk=message_id) 
+    return render_to_response('pmessages/detail.html', {'message': m})
--- proxy/urls.py
+++ proxy/urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'pmessages.views.index'),
+    url(r'^m/', include('pmessages.urls')),
 )
