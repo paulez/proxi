1d25217894c1cef41d8c6b075e4ed6e5cd5058f0
Paul Ezvan <paul@ezvan.fr>
2012-09-01 21:25:36 +0200

    Message form.

--- pmessages/views.py
+++ pmessages/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.forms import ModelForm
+from django.template import RequestContext
 from pmessages.models import ProxyMessage
+from django.contrib.gis.utils import GeoIP
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = ProxyMessage
+        fields = ('username', 'message')
 
 def index(request):
     all_messages = ProxyMessage.objects.all().order_by('-date')
-    return render_to_response('pmessages/index.html', {'all_messages': all_messages,})
+    message_form = MessageForm()
+    return render_to_response('pmessages/index.html', {'all_messages': all_messages, 'message_form' : message_form}, context_instance=RequestContext(request))
     
 def detail(request, message_id):
     m = get_object_or_404(ProxyMessage, pk=message_id) 
     return render_to_response('pmessages/detail.html', {'message': m})
+    
+def add(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            message = form.cleaned_data['message']
+            address = request.META['REMOTE_ADDR']
+            g = GeoIP(city="/usr/share/GeoIP/GeoLiteCity.dat")
+            location = g.geos(address)
+            m = ProxyMessage(username = username, message = message, address = address, location = location)
+            m.save()
+    return HttpResponseRedirect(reverse('pmessages.views.index'))
