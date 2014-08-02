c4eabc30e767b11320f358feb3bed6c21a0079ce
Paul Ezvan <paulezva@amazon.com>
2014-08-02 11:06:17 +0100

    Use relative paths.

--- proxy/settings.py
+++ proxy/settings.py
@@ -77,7 +77,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/paul/data/logiciels/django/proxy/static/',
+    os.path.join(BASE_DIR, '../static/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -117,7 +117,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/paul/data/logiciels/django/proxy/templates',
+    os.path.join(BASE_DIR, '../templates/'),
 )
 
 INSTALLED_APPS = (
