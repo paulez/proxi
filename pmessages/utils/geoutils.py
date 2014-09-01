0d5016e6d3a2dee6a3a5ba9c372050d5b38f1f7d
Paul Ezvan <paul@ezvan.fr>
2014-09-01 22:06:42 +0000

    Split GeoUtil class into a separate module.

---
+++ pmessages/utils/geoutils.py
@@ -0,0 +1,29 @@
+from django.contrib.gis.geoip import GeoIP
+
+class GeoUtils:
+    def __init__(self):
+        self.g = GeoIP()
+        
+    def get_point_from_ip(self, ip):
+        # Return geo point corresponding to ip
+        return self.g.geos(ip)
+
+    def get_user_location_address(self, request):
+        # get user possible ip address list and return first associated location found and associated address
+        address = self.get_user_address_list(request)
+        for ip in address:
+            loc = self.get_point_from_ip(ip)
+            if loc:
+                return (loc, ip)
+        error('Cannot locate user from adress %s.', address)
+        return (None, None)
+        
+    def get_user_address_list(self, request):
+        # get a list of possible user ip address from request
+        try:
+            ip = request.META['HTTP_X_FORWARDED_FOR']
+            ip = ip.split(",")
+            return [x.strip() for x in ip]
+            
+        except KeyError:
+            return [request.META['REMOTE_ADDR']]    
