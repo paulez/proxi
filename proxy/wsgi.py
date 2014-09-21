607afd56e1b8929c72d6a810f0206f11892de2e8
Paul Ezvan <paul@ezvan.fr>
2014-09-21 19:03:51 +0000

    WSGI setup.

--- proxy/wsgi.py
+++ proxy/wsgi.py
@@ -14,8 +14,21 @@ framework.
 
 """
 import os
+import sys
+import site
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/var/proxy/local/lib/python2.7/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/var/proxy/src/proxy')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proxy.settings")
+
+
+# Activate your virtual env
+activate_env=os.path.abspath("/var/proxy/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
