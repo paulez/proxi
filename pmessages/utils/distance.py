4102c8dfde330a93e9a196d524b84c6ad2d0cd43
Paul Ezvan <paul@ezvan.fr>
2018-10-08 03:56:37 +0200

    Use min radius value as min distance to display

--- pmessages/utils/distance.py
+++ pmessages/utils/distance.py
@@ -2,6 +2,7 @@
 """
 
 import logging
+from django.conf import settings
 
 from math import log10, floor, ceil
 
@@ -15,8 +16,11 @@ def rounded_distance(distance):
     """Return the distance ceiled to its most significant digit.
     Minimum value is 100.
     """
+    def dceil(distance_meters):
+        return ceil(round(distance_meters, -int(floor(log10(abs(distance_meters))))))
+    min_distance = dceil(settings.PROXY_RADIUS_MIN)
     dist = distance.m
     if dist == 0:
-        return 100
-    ceiled_distance = ceil(round(dist, -int(floor(log10(abs(dist))))))
-    return max(ceiled_distance, 100)
+        return min_distance
+    ceiled_distance = dceil(dist)
+    return max(ceiled_distance, min_distance)
