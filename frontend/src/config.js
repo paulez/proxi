7d6a87b8b689e1789ea765bdd8f02415bd31dadb
Paul Ezvan <paul@ezvan.fr>
2018-10-10 08:09:24 +0200

    Only use dev settings in dev.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -6,8 +6,8 @@ const prod = {
   api: "https://api.prxi.net/",
 };
 
-const config = process.env.REACT_APP_STAGE === 'production'
-  ? prod
-  : dev;
+const config = process.env.REACT_APP_STAGE === 'dev'
+  ? dev
+  : prod;
 
   export default config;
