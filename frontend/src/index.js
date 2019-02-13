858bdbf3959e854c17ce8493d6f14b392058c827
Paul Ezvan <paul@ezvan.fr>
2019-02-13 03:39:50 -0800

    Load bootstrap css for offline dev.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import ProxyAbout from './About.js';
 import registerServiceWorker from './registerServiceWorker';
