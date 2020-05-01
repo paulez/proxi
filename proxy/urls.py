ae609ba55a08a9a516cc75dc77243d091423da62
Paul Ezvan <paul@ezvan.fr>
2020-05-01 17:13:28 +0200

    Fix url admin error

--- proxy/urls.py
+++ proxy/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     #url(r'^$', pmessages.views.index, name='index'),
     url(r'', include('pmessages.urls', namespace='pmessages')),
 ]
